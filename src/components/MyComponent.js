2d3393d37ffd9af3ebf18e2705e447369f3b87db
quyennb <quyen.nb174147@sis.hust.edu.vn>
2024-05-17 15:10:47 +0700

    21. Form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -24,13 +24,26 @@ class MyComponent extends React.Component {
         console.log(event)
     }
 
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     //JSX
     render() {
         return(
             <div>
                 My name is {this.state.name} and I'm {this.state.age}
-                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
-                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input type="text" onChange={(event) => this.handleOnChangeInput(event)}></input>
+                    <button>Submit</button>
+                </form>
             </div>
         );
     };

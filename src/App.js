778e1818cb6d24fcdde29a5f13007e09a269adf9
quyennb <quyen.nb174147@sis.hust.edu.vn>
2024-05-29 10:38:31 +0700

    48. Điều hướng trang với Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header></Header>
+      <Header />
+      <div>
+          Test link
+          <div>
+            <button>
+              <Link to={"/admins"}>Go to admin page</Link>
+            </button>
+            <button>
+              <Link to={"/users"}>Go to user page</Link>
+            </button>
+          </div>
+        </div>
     </div>
   );
 }

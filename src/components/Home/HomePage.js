faadb36bc74feea66f76d4073d50126b9885dd38
quyennb <quyen.nb174147@sis.hust.edu.vn>
2024-05-29 12:04:40 +0700

    52. Design Homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,14 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
+
 const HomePage = (props) => {
     return (
         <div className="homepage-cotainer">
-            Home page component
+            <video autoPlay muted loop>
+                <source 
+                    src={videoHomepage}
+                    type="video/mp4">
+                </source>
+            </video>
         </div>
     )
 }

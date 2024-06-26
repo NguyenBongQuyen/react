dbe66f1dfd3acff8abaa6f40ed8e1977209413e6
quyennb <quyen.nb174147@sis.hust.edu.vn>
2024-06-26 14:56:40 +0700

    62. Cập nhật danh sách Users khi thêm mới

--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
-import { getAllUser } from "../../../services/apiService";
+
 
 const TableUser = (props) => {
-    const [listUsers, setListUsers] = useState([])
-
-    //componentDidMount
-    useEffect(() => {
-        fetchListUsers();
-    }, []);
-
-    const fetchListUsers = async() => {
-        let response = await getAllUser();
-        if (response.EC === 0) {
-            setListUsers (response.DT)
-        }
-    }
+    const {listUsers} = props;
+    // const listUsers = props.listUsers;
 
     return (
         <>
             <table className="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th scope="col">No</th>
+                    <th scope="col">ID</th>
                     <th scope="col">Username</th>
                     <th scope="col">Email</th>
                     <th scope="col">Role</th>
@@ -33,7 +21,7 @@ const TableUser = (props) => {
                     listUsers.map((item, index) => {
                         return (
                             <tr key={`table-users-${index}`}>
-                                <td>{index + 1}</td>
+                                <td>{item.id}</td>
                                 <td>{item.username}</td>
                                 <td>{item.email}</td>
                                 <td>{item.role}</td>

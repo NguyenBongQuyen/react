91d46ce48ffd1a89faad62da81f89d092218c88d
quyennb <quyen.nb174147@sis.hust.edu.vn>
2024-08-07 11:15:22 +0700

    78. Sử dụng Redux lưu thông tin người dùng

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import { NavLink, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const account = useSelector(state => state.user.account);
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+    
     const navigate = useNavigate();
 
     const handleLogin = () => {
@@ -30,13 +35,17 @@ const Header = () => {
                         <Nav.Link href="/users">User</Nav.Link> */}
                     </Nav>
                     <Nav>
-                        <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
-                        <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
-                        {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item>Log in</NavDropdown.Item>
-                            <NavDropdown.Item>Log out</NavDropdown.Item>
-                            <NavDropdown.Item>Profile</NavDropdown.Item>
-                        </NavDropdown> */}
+                        {isAuthenticated === false ?
+                            <>
+                                <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
+                                <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
+                            </>
+                            :                     
+                            <NavDropdown title="Setting" id="basic-nav-dropdown">
+                                <NavDropdown.Item>Log out</NavDropdown.Item>
+                                <NavDropdown.Item>Profile</NavDropdown.Item>
+                            </NavDropdown>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
